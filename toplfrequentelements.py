b29dec39beff6a31771cf24f19577623f5e319cd
xushen <xs286@cornell.edu>
2018-04-11 20:55:08 -0400

    first commit

---
+++ toplfrequentelements.py
@@ -0,0 +1,16 @@
+# Solution1: sort by frequency+pick top k
+# Time complexity: O(nlogn)
+# Space complexity: O(n)
+
+# use hash tap to store the elements frequency
+# scan the array O(n)
+
+# define a bucket, the key is frequency, the value is all the numbers with
+# that specific frequency
+
+
+# define ans
+# scan the ans
+# bucket insert elements into answer
+
+
