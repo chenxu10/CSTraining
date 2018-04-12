ead7bfd2f0bd4b095012710ced0b18f6a9d6aa46
xushen <xs286@cornell.edu>
2018-04-12 09:22:12 -0400

    first commit

--- toplfrequentelements.py
+++ toplfrequentelements.py
@@ -2,8 +2,16 @@
 # Time complexity: O(nlogn)
 # Space complexity: O(n)
 
+# 偷懒的调包方法
+import collections
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        return [i[0] for i in collections.Counter(nums).most_common(k)]
+
+# 从头开始实现
 # use hash tap to store the elements frequency
 # scan the array O(n)
+
 
 # define a bucket, the key is frequency, the value is all the numbers with
 # that specific frequency
